73f05221e5e56d0dec1010a9dafa035930efb213
Andrew Waters <ccfreak2k@gmail.com>
2012-09-23 08:40:05 -0700

    Exit early if we couldn't allocate a buffer.

--- util/padimage.c
+++ util/padimage.c
@@ -17,6 +17,10 @@ int main( int argc, char **argv )
 	b = ftell( f );
 	b = (b/128+1) * 128 - b;
 	zerobuf = calloc( 1, b );
+	if ( zerobuf == NULL ) {
+		printf( "Failed to allocate file buffer\n" );
+		return 1;
+	}
 	fwrite( zerobuf, 1, b, f );
 	printf( "%d sectors.\n", (int)ftell( f ) / 128 );
 	fclose( f );
