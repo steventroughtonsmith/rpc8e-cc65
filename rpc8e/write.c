6f080b2fe616ff8b5fbe7e7f94153119e133a7e9
Eric Kern <ekern256@gmail.com>
2013-03-25 13:15:28 -0400

    Reverted previous write change, since it wasn't faster for most cases.

--- rpc8e/write.c
+++ rpc8e/write.c
@@ -30,64 +30,6 @@
 
 int __fastcall__ write ( int fd, const void* buf, unsigned count )
 {
-    char *cbuf = (char *)buf;
-    unsigned char x, y, i, j, linelen;
-    
-    x = PEEK(0x301);
-    y = PEEK(0x302);
-    POKE(0x300, y);
-
-    i = 0;
-    while (i < count) {
-        linelen = count - i;
-        linelen = (linelen > (80-x) ? (80-x) : linelen);
-        for (j = 0; j < linelen; ++j)
-        {
-            if (cbuf[i] == '\n')
-            {
-                x = 80;
-                ++i;
-                break;
-            }
-            else
-            {
-                POKE(0x310 + x, cbuf[i++]);
-                POKE(0x301, ++x);
-            }
-        }
-
-        if (x >= 80)
-        {
-            if (y >= 49)
-            {
-                POKE(0x30a, 0);
-                POKE(0x30b, 0);
-                POKE(0x308, 0);
-                POKE(0x309, 1);
-                POKE(0x30c, 80);
-                POKE(0x30d, 49);
-                POKE(0x307, 3);
-                while (PEEK(0x307) == 3);
-
-                for (j = 0; j < 80; ++j)
-                    POKE(0x310 + j, 0x20);
-            }
-            else
-            {
-                POKE(0x302, ++y);
-                POKE(0x300, y);
-            }
-            
-            x = 0;
-            POKE(0x301, x);
-        }
-    }
-
-    return count;
-}
-
-int __fastcall__ write_old ( int fd, const void* buf, unsigned count )
-{
     unsigned i = 0;
 	char *cbuf = (char *) buf;
 
